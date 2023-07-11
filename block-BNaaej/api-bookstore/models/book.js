cbc54d5d98764e1e9d43326016fa8468f2640961
Venky0003 <rajv17270@gmail.com>
2023-07-11 15:41:26 +0530

    add updated api-bookstore with category and tags

---
+++ block-BNaaej/api-bookstore/models/book.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  categories:  [String],
+  tags: [String],
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+});
+
+var Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
+  
+
